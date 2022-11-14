e703351e9c24c02c856177c464f76aae392d0827
joshuatean <105474970+joshuatean@users.noreply.github.com>
2022-11-14 13:48:34 +1100

    Initial commit
    First commit

---
+++ static/js/app.js
@@ -0,0 +1,15 @@
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+function initialize()   {
+    let dropdown = d3.select("#selDataset");
+    d3.json(url).then(function(data)    {
+        console.log(data);
+        let samples = data.names;
+        samples.forEach((detail) => {
+            dropdown.append("option").text(detail).property("value", detail);
+        });
+    drawcharts(samples[0]);
+    });
+};
+
+initialize();
